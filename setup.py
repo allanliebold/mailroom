1738ed25ded6fdaa798340d29cc8ace53d95bcdd
David Franklin <dave5801@gmail.com>
2017-10-22 13:20:32 -0700

    created setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        name="mailroom"
+        description="A Python implementaiton of MailRoom Madness",
+        version=0.1,
+        author="David Franklin, Allan Liebold",
+        licence="MIT",
+        py_modules=['mailroom']
+    )
