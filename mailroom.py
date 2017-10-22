dae74ab1e36503135a31fec4685e01e233df1123
Allan Liebold <allan.liebold@gmail.com>
2017-10-22 13:47:52 -0700

    adds docstrings and formatting.

--- mailroom.py
+++ mailroom.py
@@ -1,18 +1,16 @@
+"""Mailroom Madness."""
 donar_list = {}
 
 def main():
-    
+    """Main Function."""
     running = True
 
-    while(running == True):
+    while(running is True):
         print("Hello, Welcome to Mail Room Madness") 
 
         option_select = input("Options 1) Send 'Thank You' 2) Create Report or 'q' to quit.")
-        
-
         if option_select == 'q':
             running = False
-
         elif option_select == '1':
             print(send_thanks())
         elif option_select == '2':
@@ -20,11 +18,16 @@ def main():
 
     print("END")
 
+
 def send_thanks():
+    """Send Thank You."""
     return "THANK YOU"
 
+
 def create_report():
+    """Create Report."""
     return "REPORT CREATED"
 
-if __name__ == '__main__':
+
+if __name__ == '__main__': # pragma : no cover
     main()
