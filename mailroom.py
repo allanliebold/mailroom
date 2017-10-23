f670c838986a38758f2eb915cf0c15afb9a41295
Allan Liebold <allan.liebold@gmail.com>
2017-10-23 08:59:52 -0700

    cleans up formatting, adds the option to quit back to initial prompt, and accounts for some prompt edge cases.

--- mailroom.py
+++ mailroom.py
@@ -1,25 +1,28 @@
 """Mailroom Madness."""
-donar_list = {'John Snow': ['100', '20', '50'], 'Robert Stark': ['50', '30', '10'],
+donar_list = {'John Snow': ['100', '20', '50'],
+              'Robert Stark': ['50', '30', '10'],
               'Tyrion Lannister': ['500', '600']}
-donar_report = {}
+
 
 def main():
     """Main Function."""
     running = True
 
     while(running is True):
-        print("Hello, Welcome to Mail Room Madness") 
-
-        option_select = input("Options 1) Send 'Thank You' 2) Create Report or 'q' to quit. ")
+        option_message = """
+        "1) Send 'Thank You' 2) Create Report or 'q' to quit. "
+        """
+        option_select = input(option_message)
         if option_select == 'q':
             running = False
         elif option_select == '1':
             donar_name = send_thanks()
-            if donar_name != False:
+            if donar_name is not False:
                 donation_amount = add_donation(donar_name)
-                print("E-mail Message: ")
-                print(create_email(donar_name, donation_amount))
-                print("SENT")
+                if donation_amount is not False:
+                    print("E-mail Message: ")
+                    print(create_email(donar_name, donation_amount))
+                    print("SENT")
         elif option_select == '2':
             print(create_report())
 
@@ -30,9 +33,9 @@ def send_thanks():
     """Send Thank You."""
     donar_name = input("Please enter a full name: ")
 
-    if check_name(donar_name)==False:
+    if check_name(donar_name) is False:
         add_name_option = input("Name %s is not in list, do you want to add to donar list? y) n) " % donar_name)
-        if add_name_option == 'n':
+        if add_name_option == 'n' or add_name_option == 'q':
             return False
         elif add_name_option == 'y':
             donar_list[donar_name] = []
@@ -43,23 +46,24 @@ def send_thanks():
 
 def check_name(donar_name):
     """Check Name function."""
-    if donar_name in donar_list:
-        return True
-    else:
+    if donar_name not in donar_list or donar_name == 'q':
         return False
+    return True
 
 
 def add_donation(donar_name):
-
+    """Add Donation function."""
     make_donation = input("Enter Donation Amount: ")
-    if check_donation(make_donation) == True:
+    if check_donation(make_donation) is True:
         donar_list[donar_name].append(int(make_donation))
         return make_donation
     else:
         print("invalid donation")
+        return False
 
 
 def check_donation(donation):
+    """Check Donation amount."""
     try:
         int(donation)
         return True
@@ -68,13 +72,14 @@ def check_donation(donation):
 
 
 def create_email(donar_name, donation_amount):
+    """Create e-mail text."""
     return "Thank you %s for your donation of $%s" %(donar_name, donation_amount)
 
 
 def create_report():
     """Create Report."""
     print("Creating Report")
-
+    donar_report = {}
     report_body = "Name of Donar     Total Donations     Average Donation\n"
 
     for donar, donations in donar_list.items():
@@ -90,14 +95,16 @@ def create_report():
     for i in temp:
         for donar in donar_report:
             if donar_report[donar][0] == i:
-                report_body += "%s          %s             %s\n" %(donar,donar_report[donar][0],donar_report[donar][1]) 
+                report_body += "%s              %s                 %s\n" %(donar,donar_report[donar][0],donar_report[donar][1]) 
     return(report_body)
 
 
 def get_average_donation(donations):
+    """Get average donation."""
     average = float(sum(donations)) / max(len(donations), 1)
     return "{0:.2f}".format(average)
 
 
 if __name__ == '__main__': # pragma : no cover
+    print("Hello, Welcome to Mail Room Madness")
     main()
