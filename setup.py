0564d06aa6d5f36f73bb7c01b316701454a29144
Allan Liebold <allan.liebold@gmail.com>
2017-10-23 08:42:00 -0700

    adds package_dir line to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(
-        name="mailroom"
-        description="A Python implementaiton of MailRoom Madness",
-        version=0.1,
-        author="David Franklin, Allan Liebold",
-        licence="MIT",
-        py_modules=['mailroom']
-    )
+    name="mailroom",
+    description="A Python implementaiton of MailRoom Madness",
+    version=0.1,
+    author="David Franklin, Allan Liebold",
+    licence="MIT",
+    py_modules=['mailroom'],
+    package_dir={'': 'src'}
+)
